b8c88df8606d4fd9810fcf7088604b392cfc1d1f
Admin <admin@users-MacBook-Air.local>
2021-08-11 14:40:50 +0200

    Desafío clase 3

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return (
+        <>
+        <header>
+            <h1>Desafío clase 3</h1>
+            <h2>Rodrigo Monteoliva</h2>
+            <nav>
+                <ul>
+                <li><a href="https://www.coderhouse.es/">Main</a></li>
+                <li><a href="https://www.coderhouse.es/">Nosotros</a></li>
+                <li><a href="https://www.coderhouse.es/">Tienda</a></li>
+                <li><a href="https://www.coderhouse.es/">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default NavBar
